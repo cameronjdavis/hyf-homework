09f4a2b8ca7d987c3cc5e908a34fc2d4fb469587
Cameron Davis <cameronjohndavis@gmail.com>
2021-02-19 15:59:47 +0100

    Teaching js2 w2

---
+++ javascript/javascript2/week2/arrays.js
@@ -0,0 +1,67 @@
+const mentors = [
+    { "name": "Abed Sujan", "subjects": ['JS', 'HTML', 'CSS', 'NODEJS'], yearOfExperience: 4},
+    { "name": "Ahmed Magdy", "subjects": ['JS', 'Database', 'CSS'], yearOfExperience: 1},
+    { "name": "Alicia Gonzales", "subjects": ['DB', 'HTML', 'NODEJS'], yearOfExperience: 8},
+    { "name": "allan Thraen", "subjects": ['REACT', 'HTML', 'CSS'], yearOfExperience: 3},
+    { "name": "Anders Ravn", "subjects": ['JS', 'HTML', 'NODEJS'], yearOfExperience: 2},
+    { "name": "Daniel Fernandes", "subjects": ['Database', 'HTML', 'CSS'], yearOfExperience: 9},
+    { "name": "Cameron Davis", "subjects": ['Database', 'CSS', 'Fun'], yearOfExperience: 9},
+];
+
+// const filteredMentors = mentors.reduce((filteredMentors, mentor) => mentor.yearOfExperience >= 9 ? filteredMentors.concat([mentor]) : filteredMentors, [])
+// console.log(filteredMentors)
+
+// const filteredMentors = mentors.reduce(function(filteredMentors, mentor) {
+//     return mentor.yearOfExperience >= 9 ? filteredMentors.concat([mentor]) : filteredMentors
+// }, [])
+// console.log(filteredMentors)
+
+// const filteredMentors = mentors.reduce(function(filteredMentors, mentor) {
+//     if(mentor.yearOfExperience > 6) {
+//         filteredMentors.push(mentor)
+//     }
+//     return filteredMentors
+// }, [])
+// console.log(filteredMentors)
+
+// const totalYearsExperience = mentors.reduce(function(totalYearsExperience, mentor) {
+//     return totalYearsExperience += mentor.yearOfExperience
+// }, 0)
+// console.log(totalYearsExperience)
+
+// let totalYearsExperience = 0
+// for(mentor of mentors) {
+//     totalYearsExperience += mentor.yearOfExperience
+// }
+// console.log(totalYearsExperience)
+
+// const sortedMentors = mentors.sort(function(mentor1, mentor2) {
+//     return mentor1.yearOfExperience < mentor2.yearOfExperience ? 1 : -1
+// })
+// console.log(sortedMentors)
+
+// const experiencedMentors = mentors.filter(function(mentor) {
+//     return mentor.yearOfExperience > 6
+// })
+// console.log(experiencedMentors)
+
+// const introductions = mentors.map(function(mentor) {
+//     return "Mentor's name is " + mentor.name
+// })
+// console.log(introductions)
+
+// mentors.forEach(function(mentor) {
+//     console.log(mentor.name)
+// })
+
+// for(mentor of mentors) {
+//     console.log(mentor.name)
+// }
+
+// for(let i=0; i<mentors.length; i++) {
+//     console.log(mentors[i].name)
+// }
+
+// for
+// for of
+// forEach
---
+++ javascript/javascript2/week2/exercise1.js
@@ -0,0 +1,61 @@
+/**
+ * Get random integer between two numbers, found here: https://stackoverflow.com/a/7228322
+ * @param {integer} min - The min number
+ * @param {integer} max - The max number
+ * @returns {Number} Random number between min and max
+ */
+function randomIntFromInterval(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+
+/**
+ * Get an array with listing objects with random color and speed
+ * @param {integer} numberOfListings - The number of listings 
+ * @returns {array} Array containing the listing objects
+ */
+function generateListings(numberOfListings) {
+    const listings = [];
+
+    const listingType = ['House', 'Apartment', 'Shed', 'Dorm', 'Farm'];
+    const listingFacilities = ['Parkering', 'Elevator', 'Altan', 'Have', 'Husdyr'];
+
+    for (let i = 0; i < numberOfListings; i++) {
+        const listing = {};
+        const randomTypeIndex = randomIntFromInterval(0, listingType.length - 1);
+        const numberOfFacilities = randomIntFromInterval(1, listingFacilities.length - 1);
+        const facilities = [];
+        for (let i = 0; i < numberOfFacilities; i++) {
+            const randomIndexFacilities = randomIntFromInterval(0, listingFacilities.length - 1);
+            const randomFacility = listingFacilities[randomIndexFacilities];
+
+            if (!(facilities.includes(randomFacility))) {
+                facilities.push(randomFacility);
+            }
+        }
+
+        listing.type = listingType[randomTypeIndex];
+        listing.facilities = facilities;
+        listing.price = randomIntFromInterval(1, 10000);
+        listing.hasGarden = Boolean(randomIntFromInterval(0, 1));
+        listing.size = randomIntFromInterval(12, 1000);
+        listing.img = `https://loremflickr.com/200/200/${listing.type}`
+
+        listings.push(listing);
+    }
+
+    return listings;
+}
+
+const listings = generateListings(20);
+console.log(listings)
+
+// ForEach
+// Create 37 listings and log out every listings size
+// Map
+// Create an array that contains all the 37 listing prices.
+// Filter
+// Using the 37 listings from the previous tasks
+// Create an array of cheap listings. You define what cheap means. Each item in this array should be of type object
+// Reduce
+// Sum the total price of all listings
