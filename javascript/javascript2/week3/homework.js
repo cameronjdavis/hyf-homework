c6b9bb096a55ebdb59e8d44e7d07c2ef72577244
Cameron Davis <cameronjohndavis@gmail.com>
2021-02-27 18:14:59 +0100

    JS2 W3 homework

--- javascript/javascript2/week3/homework.js
+++ javascript/javascript2/week3/homework.js
@@ -52,3 +52,31 @@ document.addEventListener('dblclick',  () => {
         console.log('double click!')
     // }
 })
+
+
+// Create funtions that are used in these different ways:
+function func1() {console.log('func1')}
+function func2() {console.log('func2')}
+function func3() {console.log('func3')}
+
+// Create an array with 3 items. All items should be functions. Iterate through the array and call all the functions.
+const funcs = [
+    func1,
+    func2,
+    func3,
+];
+for(func of funcs) {
+    func()
+}
+
+// Create a function as a const and try creating a function normally. Call both functions.
+// Read up on this if you are interested:
+// https://stackoverflow.com/questions/1013385/what-is-the-difference-between-a-function-expression-vs-declaration-in-javascrip
+
+// Create an object that has a key whose value is a function. Try calling this function.
+const obj = {
+    func: function() {
+        console.log('object func')
+    }
+}
+obj.func()
